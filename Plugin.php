9eee817e321d220b64113ea7c16bc4cd0ee96c46
endoffight <endoffight@qq.com>
2015-05-08 14:29:03 +0800

    add files

---
+++ Plugin.php
@@ -0,0 +1,75 @@
+<?php
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+/**
+ * CDN助手
+ *
+ * @package CdnHelper
+ * @author 老高
+ * @version 0.1
+ * @link http://www.phpgao.com
+ */
+class CdnHelper_Plugin implements Typecho_Plugin_Interface
+{
+    /**
+     * 激活插件方法,如果激活失败,直接抛出异常
+     *
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function activate()
+    {
+        Typecho_Plugin::factory('Widget_Abstract_Contents')->contentEx = array('CdnHelper_Plugin', 'replace');
+    }
+
+    /**
+     * 禁用插件方法,如果禁用失败,直接抛出异常
+     *
+     * @static
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function deactivate(){}
+
+    /**
+     * 获取插件配置面板
+     *
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form 配置面板
+     * @return void
+     */
+    public static function config(Typecho_Widget_Helper_Form $form)
+    {
+        $siteUrl = Helper::options()->siteUrl;
+        /** 分类名称 */
+        $domain = new Typecho_Widget_Helper_Form_Element_Text('domain', null, $siteUrl, _t('图片域名'), '一般为主域名');
+        $form->addInput($domain);
+
+        $cdn = new Typecho_Widget_Helper_Form_Element_Text('cdn', null, $siteUrl, _t('CDN加速域名'), '格式请与图片域名对应，不要忘了http和最后的/');
+        $form->addInput($cdn);
+    }
+
+    /**
+     * 个人用户的配置面板
+     *
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form
+     * @return void
+     */
+    public static function personalConfig(Typecho_Widget_Helper_Form $form){}
+
+    /**
+     * 插件实现方法
+     *
+     * @access public
+     * @return void
+     */
+    public static function replace($string, $class, $c)
+    {
+        $options = Helper::options()->plugin('CdnHelper');
+        $pattern = '#(<img\s+src=")(' . $options->domain .')([^<]*?/>)#i';
+        $replacement = '${1}' . $options->cdn . '${3}';
+        return @preg_replace($pattern, $replacement, $string);
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# CDNhelper
+ A typecho plugin for cdn
+
+
+主要功能是替换了文章中的img-src为加速后的src
+
+功能待完善
